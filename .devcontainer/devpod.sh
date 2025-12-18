d1d9739051d1c914985c40434a727454940bc105
Mike Valeriano <mkvlrn@gmail.com>
2025-12-18 00:53:06 -0300

    chore: update devcontainer

--- .devcontainer/devpod.sh
+++ .devcontainer/devpod.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ENV_FILE="./.devcontainer/.env.devcontainer"
 RECREATE_FLAG=""
@@ -7,21 +7,27 @@ if [[ "$1" == "--recreate" ]]; then
   RECREATE_FLAG="--recreate"
 fi
 
-if [ ! -f "$ENV_FILE" ]; then
-  echo "error: environment file '$ENV_FILE' not found." >&2
-  exit 1
-fi
-
 command -v devpod-cli >/dev/null 2>&1 && [ -x "$(command -v devpod-cli)" ] ||
   {
     echo "error: devpod-cli not found or not executable in PATH" >&2
     exit 1
   }
 
-source_command=$(grep -v '^#' "$ENV_FILE" | xargs -I {} echo "export {}")
-eval "$source_command"
+if [ ! -f "$ENV_FILE" ]; then
+  echo "error: environment file '$ENV_FILE' not found." >&2
+  exit 1
+fi
+
+while IFS='=' read -r key value; do
+  [[ -z "$key" || "$key" =~ ^# ]] && continue
+  value="${value#\"}"
+  value="${value%\"}"
+  value="${value#\'}"
+  value="${value%\'}"
+  export "$key=$value"
+done <"$ENV_FILE"
 
 devpod-cli context set-options default -o SSH_INJECT_GIT_CREDENTIALS=false
-devpod-cli up . --ide "$PROJECT_EDITOR" \
-  --workspace-env-file "$ENV_FILE" \
-  "$RECREATE_FLAG"
+CMD=(devpod-cli up . --ide "$PROJECT_EDITOR" --workspace-env-file "$ENV_FILE")
+[[ -n "$RECREATE_FLAG" ]] && CMD+=("$RECREATE_FLAG")
+"${CMD[@]}"
